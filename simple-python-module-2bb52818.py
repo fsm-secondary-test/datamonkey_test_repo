de272d136628526917ed254e5cd782eec0ee64e0
fsm-secondary-test <fsmsecondary@gmail.com>
2025-08-16 17:42:48 +0200

    Add datamonkey test file: simple-python-module-2bb52818.py

---
+++ simple-python-module-2bb52818.py
@@ -0,0 +1,10 @@
+"""A minimal Python module with one function and one class; includes tracking token in docstrings. | token=2bb52818"""
+
+return f"Hello, {name}!"
+
+def __init__(self, start: int = 0):
+    self.value = start
+
+def increment(self, step: int = 1) -> int:
+    self.value += step
+    return self.value
