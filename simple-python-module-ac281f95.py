4d0118d167b492642dd3552bbc87f6817bbb7544
fsm-secondary-test <fsmsecondary@gmail.com>
2025-08-16 19:51:24 +0200

    Update datamonkey test file: simple-python-module-ac281f95.py

--- simple-python-module-ac281f95.py
+++ simple-python-module-ac281f95.py
@@ -1,4 +1,4 @@
-"""A tiny module with one function and one class; includes the token in a docstring. | token=ac281f95"""
+"""A minimal Python module with one function and one class, including the token in a docstring and constant. | token=ac281f95"""
 
 
 
