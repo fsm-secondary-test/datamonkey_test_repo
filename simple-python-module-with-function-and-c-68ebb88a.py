bc6df78d55d20527b5337d61f8438c77f49a441a
fsm-secondary-test <fsmsecondary@gmail.com>
2025-08-17 18:34:18 +0200

    Add monke test file: simple-python-module-with-function-and-c-68ebb88a.py

---
+++ simple-python-module-with-function-and-c-68ebb88a.py
@@ -0,0 +1,5 @@
+"""A minimal Python module featuring one function and one class. The literal token is embedded in a docstring and a string constant. | token=68ebb88a"""
+
+return f"Hello, {name}!"
+
+
