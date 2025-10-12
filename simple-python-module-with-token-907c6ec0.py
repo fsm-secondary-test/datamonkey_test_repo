224f895734a84cd7b7f151639e3c4d33bb1a87dd
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-12 11:42:20 +0200

    Add monke test file: simple-python-module-with-token-907c6ec0.py

---
+++ simple-python-module-with-token-907c6ec0.py
@@ -0,0 +1,16 @@
+"""A small Python module containing one simple function and one simple class. The token '907c6ec0' is included in a docstring. | token=907c6ec0"""
+
+def greet(name):
+    """Return a greeting message."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """Simple counter class with token 907c6ec0 in docstring."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def get_count(self):
+        return self.count
