68b7b4418f8a6275ebc8194447433eaf953d6fbe
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-23 16:12:54 +0200

    Add monke test file: simple-module-with-token-8e760e8f.py

---
+++ simple-module-with-token-8e760e8f.py
@@ -0,0 +1,13 @@
+"""This Python module contains a simple function and a simple class. It includes the literal token '8e760e8f' in the class docstring. | token=8e760e8f"""
+
+def greet(name):
+    """Return a greeting message for the given name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class that holds the token '8e760e8f' as a constant."""
+    TOKEN = '8e760e8f'
+
+    def get_token(self):
+        """Return the token."""
+        return self.TOKEN
