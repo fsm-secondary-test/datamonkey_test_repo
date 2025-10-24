a1cc7286b2a9ddc60f30589f79dd3cc2447a579c
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-24 12:22:08 +0200

    Update monke test file: simple-python-module-582e99de.py

--- simple-python-module-582e99de.py
+++ simple-python-module-582e99de.py
@@ -1,13 +1,17 @@
-"""A small Python module with one function and one class, including the token '582e99de' in a string constant. | token=582e99de"""
+"""A small Python module containing one function and one class with the token '582e99de' included in a docstring. | token=582e99de"""
 
 def greet(name):
-    """Return a greeting message for the given name."""
+    """Return a greeting message including the given name."""
     return f"Hello, {name}!"
 
-class TokenHolder:
-    """A simple class holding a constant token."""
-    TOKEN = '582e99de'
+class Counter:
+    """A simple counter class with token 582e99de."""
+    def __init__(self):
+        self.count = 0
 
-    def get_token(self):
-        """Return the stored token."""
-        return self.TOKEN
+    def increment(self):
+        self.count += 1
+        return self.count
+
+    def reset(self):
+        self.count = 0
