145304c5819f388dcfa2010c507e997dfca59934
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-24 10:18:42 +0200

    Update monke test file: simple-python-module-f3ef02b8.py

--- simple-python-module-f3ef02b8.py
+++ simple-python-module-f3ef02b8.py
@@ -1,17 +1,14 @@
-"""A small Python module containing one function and one class. Includes a token in the docstring. | token=f3ef02b8"""
+"""A small Python module with one function and one class including a specific token. | token=f3ef02b8"""
 
 def greet(name):
-    """Return a greeting message for the given name. Token: f3ef02b8"""
-    return f"Hello, {name}!"
+    """Return a greeting message including the token f3ef02b8."""
+    return f"Hello, {name}! Your token is f3ef02b8."
 
-class Counter:
-    """A simple counter class containing token f3ef02b8."""
-    def __init__(self):
-        self.count = 0
-
-    def increment(self):
-        self.count += 1
-        return self.count
-
-    def reset(self):
-        self.count = 0
+class Person:
+    """A simple Person class storing a name and a token f3ef02b8."""
+    def __init__(self, name):
+        self.name = name
+        self.token = 'f3ef02b8'
+    
+    def introduce(self):
+        return f"Hi, I'm {self.name} and my token is {self.token}."
