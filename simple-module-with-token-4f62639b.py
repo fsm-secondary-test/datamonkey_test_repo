7a0c303262eee41ae1e61ec88c3f1422a23a4e4d
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-26 13:37:49 +0100

    Add monke test file: simple-module-with-token-4f62639b.py

---
+++ simple-module-with-token-4f62639b.py
@@ -0,0 +1,14 @@
+"""A Python module containing one simple function and one simple class. The token '4f62639b' is included in a docstring. | token=4f62639b"""
+
+def greet(name):
+    """Return a greeting message including the token 4f62639b."""
+    return f"Hello, {name}! Your token is 4f62639b."
+
+class Counter:
+    """Simple counter class with token 4f62639b in the docstring."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
