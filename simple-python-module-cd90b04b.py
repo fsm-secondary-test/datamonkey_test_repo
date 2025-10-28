ea227c86ee333d0aa761d40f7d308ee5347f3a1d
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-28 20:51:48 +0100

    Add monke test file: simple-python-module-cd90b04b.py

---
+++ simple-python-module-cd90b04b.py
@@ -0,0 +1,16 @@
+"""A Python module containing one simple function and one simple class, including the token 'cd90b04b'. | token=cd90b04b"""
+
+def greet(name):
+    """Return a greeting message including the given name and token cd90b04b."""
+    return f"Hello, {name}! Token: cd90b04b"
+
+class Counter:
+    """A simple counter class that includes the token cd90b04b in its docstring."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def get_count(self):
+        return self.count
