7e60e02488b4bbee7e79c3efaf4280ab648dd822
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-28 20:13:41 +0100

    Update monke test file: simple-python-module-edadedc3.py

--- simple-python-module-edadedc3.py
+++ simple-python-module-edadedc3.py
@@ -1,13 +1,18 @@
-"""A small Python module with one function and one class, including the token 'edadedc3'. | token=edadedc3"""
+"""A small Python module containing one function and one class, including the token 'edadedc3'. | token=edadedc3"""
 
 def greet(name):
-    """Return a greeting message including the given name."""
-    return f"Hello, {name}!"
+    """Return a greeting message including the given name and token edadedc3."""
+    return f"Hello, {name}! Token: edadedc3"
 
-class TokenHolder:
-    """Class that holds the special token 'edadedc3'."""
-    TOKEN = 'edadedc3'
+class Counter:
+    """A simple counter class that counts up from zero. Token: edadedc3"""
+    def __init__(self):
+        self.count = 0
 
-    def get_token(self):
-        """Return the stored token."""
-        return self.TOKEN
+    def increment(self):
+        """Increment the counter by one."""
+        self.count += 1
+
+    def get_count(self):
+        """Return the current count."""
+        return self.count
