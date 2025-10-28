2700052031b2ff8a0aa2980066c3a088f26007ca
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-28 16:17:20 +0100

    Add monke test file: simple-python-module-e07160b2.py

---
+++ simple-python-module-e07160b2.py
@@ -0,0 +1,14 @@
+"""A small Python module containing one function and one class with the token 'e07160b2' included in a docstring. | token=e07160b2"""
+
+def greet(name):
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class that increments a count. Token: e07160b2"""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
