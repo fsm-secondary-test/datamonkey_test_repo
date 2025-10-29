44ea278a710f512de55618a1f40308c196a39eab
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-29 16:02:06 +0100

    Update monke test file: simple-python-module-0a41c061.py

--- simple-python-module-0a41c061.py
+++ simple-python-module-0a41c061.py
@@ -1,20 +1,14 @@
-"""A small Python module with one function and one class, including a specific token in a docstring. | token=0a41c061"""
+"""A small Python module with one function and one class, including the token '0a41c061'. | token=0a41c061"""
 
 def greet(name):
-    """Return a greeting message including the given name. Token: 0a41c061"""
+    """Return a greeting message including the given name."""
     return f"Hello, {name}!"
 
 class Counter:
-    """A simple counter class. Token: 0a41c061"""
-
+    """A simple counter class that increments a count. Token: 0a41c061"""
     def __init__(self):
         self.count = 0
 
     def increment(self):
         self.count += 1
-
-    def reset(self):
-        self.count = 0
-
-    def get_count(self):
         return self.count
