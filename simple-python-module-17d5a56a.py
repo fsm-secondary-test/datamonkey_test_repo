c4718563b4ab094e0ecca5437936a9c0be792bfa
fsm-secondary-test <fsmsecondary@gmail.com>
2025-10-29 08:35:57 +0100

    Add monke test file: simple-python-module-17d5a56a.py

---
+++ simple-python-module-17d5a56a.py
@@ -0,0 +1,14 @@
+"""A small Python module with one function and one class containing the token '17d5a56a'. | token=17d5a56a"""
+
+def add_two_numbers(a, b):
+    """Add two numbers and return the result."""
+    return a + b
+
+class Greeting:
+    """A simple greeting class containing token 17d5a56a."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def say_hello(self):
+        return f"Hello, {self.name}!"
