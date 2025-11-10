8edbf9e8e6d92ec9ce5b4e6f53a346ad0fe987da
fsm-secondary-test <fsmsecondary@gmail.com>
2025-11-10 19:36:13 +0100

    Add monke test file: simple-python-module-83ffc9a0.py

---
+++ simple-python-module-83ffc9a0.py
@@ -0,0 +1,13 @@
+"""A small Python module containing one function and one class, including the token '83ffc9a0'. | token=83ffc9a0"""
+
+def greet(name):
+    """Return a greeting message for the given name."""
+    return f"Hello, {name}!"
+
+class Person:
+    """Person class with a name attribute and a greet method. Token: 83ffc9a0"""
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        return f"Hi, I am {self.name}."
