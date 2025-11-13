c63bc3f5eefdf3ea7a9d5689329b55fdd52d73f9
fsm-secondary-test <fsmsecondary@gmail.com>
2025-11-13 19:56:59 +0100

    Update monke test file: simple-python-module-37f775ef.py

--- simple-python-module-37f775ef.py
+++ simple-python-module-37f775ef.py
@@ -1,17 +1,13 @@
-"""A small Python module containing one function and one class, including the token '37f775ef' in a docstring. | token=37f775ef"""
+"""A small Python module containing one simple function and one simple class. Includes the token '37f775ef' in a docstring. | token=37f775ef"""
 
 def greet(name):
-    """Return a greeting message including the given name."""
+    """Return a greeting message including the name."""
     return f"Hello, {name}!"
 
-class Counter:
-    """Simple counter class with token 37f775ef included."""
-    def __init__(self):
-        self.count = 0
-    
-    def increment(self):
-        self.count += 1
-        return self.count
-    
-    def reset(self):
-        self.count = 0
+class TokenHolder:
+    """Class that holds a special token '37f775ef' as a class attribute."""
+    token = '37f775ef'
+
+    def get_token(self):
+        """Return the stored token."""
+        return self.token
