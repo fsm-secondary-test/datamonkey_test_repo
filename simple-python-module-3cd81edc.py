664594fb90990ea1dc717b60b7b2b24b8f86df39
fsm-secondary-test <fsmsecondary@gmail.com>
2025-11-15 23:14:11 +0100

    Add monke test file: simple-python-module-3cd81edc.py

---
+++ simple-python-module-3cd81edc.py
@@ -0,0 +1,18 @@
+"""A small Python module containing one function and one class with the token '3cd81edc' in a docstring. | token=3cd81edc"""
+
+def greet(name):
+    """Return a greeting message including the given name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class with token 3cd81edc in the docstring."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        """Increment the counter by one."""
+        self.count += 1
+
+    def value(self):
+        """Return the current count value."""
+        return self.count
