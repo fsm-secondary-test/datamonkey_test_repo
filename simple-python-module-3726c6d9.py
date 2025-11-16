0d08e4adec973b8c52ce77f0a13440520a63beb3
fsm-secondary-test <fsmsecondary@gmail.com>
2025-11-16 04:30:17 +0100

    Add monke test file: simple-python-module-3726c6d9.py

---
+++ simple-python-module-3726c6d9.py
@@ -0,0 +1,17 @@
+"""A small Python module with one simple function and one simple class, including the token '3726c6d9'. | token=3726c6d9"""
+
+def greet(name):
+    """Return a greeting message including the token 3726c6d9."""
+    return f"Hello, {name}! Token: 3726c6d9"
+
+class Counter:
+    """A simple counter class that keeps track of a count. Token: 3726c6d9"""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
+
+    def reset(self):
+        self.count = 0
