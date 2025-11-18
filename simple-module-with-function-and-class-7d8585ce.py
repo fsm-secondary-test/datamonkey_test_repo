623d118855384469824b69fe4ae9ec64737c94c7
fsm-secondary-test <fsmsecondary@gmail.com>
2025-11-18 02:31:59 +0100

    Add monke test file: simple-module-with-function-and-class-7d8585ce.py

---
+++ simple-module-with-function-and-class-7d8585ce.py
@@ -0,0 +1,14 @@
+"""A small Python module containing one simple function and one simple class, including the literal token '7d8585ce' in a docstring. | token=7d8585ce"""
+
+def greet(name):
+    """Return a greeting message including the token 7d8585ce."""
+    return f"Hello, {name}! Token: 7d8585ce"
+
+class Counter:
+    """A simple counter class that increments a count. Token: 7d8585ce."""
+    def __init__(self):
+        self.count = 0
+    
+    def increment(self):
+        self.count += 1
+        return self.count
