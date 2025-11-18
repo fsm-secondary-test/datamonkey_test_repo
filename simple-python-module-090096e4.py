aea9176723cf659d27e2accb34596ea2528a992f
fsm-secondary-test <fsmsecondary@gmail.com>
2025-11-18 02:16:42 +0100

    Update monke test file: simple-python-module-090096e4.py

--- simple-python-module-090096e4.py
+++ simple-python-module-090096e4.py
@@ -1,17 +1,15 @@
-"""A small Python module containing one function and one class with the token '090096e4' included in a docstring. | token=090096e4"""
+"""This module contains one simple function and one simple class with the token '090096e4' included in a docstring. | token=090096e4"""
 
 def greet(name):
-    """Return a greeting message including the name and token 090096e4."""
+    """Return a greeting message including the token '090096e4'."""
     return f"Hello, {name}! Your token is 090096e4."
 
 class Counter:
-    """A simple counter class with token 090096e4."""
+    """A simple counter class. Token: 090096e4."""
+
     def __init__(self):
         self.count = 0
-    
+
     def increment(self):
         self.count += 1
         return self.count
-    
-    def reset(self):
-        self.count = 0
