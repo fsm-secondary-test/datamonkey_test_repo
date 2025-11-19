a32c93e90cdecce5262b6603fcc6e8c2c7d4fb48
fsm-secondary-test <fsmsecondary@gmail.com>
2025-11-19 19:53:15 +0100

    Add monke test file: simple-python-module-ea4fda04.py

---
+++ simple-python-module-ea4fda04.py
@@ -0,0 +1,14 @@
+"""This module contains a simple function and a simple class, including the token 'ea4fda04' in a docstring. | token=ea4fda04"""
+
+def greet(name):
+    """Return a greeting message including the token ea4fda04."""
+    return f"Hello, {name}! Token: ea4fda04"
+
+class Counter:
+    """A simple counter class with token ea4fda04 in the docstring."""
+    def __init__(self):
+        self.count = 0
+    
+    def increment(self):
+        self.count += 1
+        return self.count
