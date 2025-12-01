bce23e01b770090b7ec77e0abb640272f32903e4
fsm-secondary-test <fsmsecondary@gmail.com>
2025-12-01 23:53:18 +0100

    Add monke test file: simple-python-module-992fafd3.py

---
+++ simple-python-module-992fafd3.py
@@ -0,0 +1,17 @@
+"""A small Python module containing one function and one class, including the token '992fafd3' in a docstring. | token=992fafd3"""
+
+def greet(name):
+    """Return a greeting message including the provided name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class. Token included: 992fafd3"""
+    def __init__(self):
+        self.count = 0
+    
+    def increment(self):
+        self.count += 1
+        return self.count
+    
+    def reset(self):
+        self.count = 0
