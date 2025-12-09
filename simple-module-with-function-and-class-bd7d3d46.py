034014c08208709773648d2975a55e894540deca
fsm-secondary-test <fsmsecondary@gmail.com>
2025-12-09 20:44:37 +0100

    Add monke test file: simple-module-with-function-and-class-bd7d3d46.py

---
+++ simple-module-with-function-and-class-bd7d3d46.py
@@ -0,0 +1,16 @@
+"""A Python module containing a simple function and a simple class, including the token 'bd7d3d46' in a docstring. | token=bd7d3d46"""
+
+def greet(name):
+    """Return a greeting message including the provided name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """Simple counter class with token bd7d3d46 in docstring."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def value(self):
+        return self.count
