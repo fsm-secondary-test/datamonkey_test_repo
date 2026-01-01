132210ecf86104d90c11089aa106ea582ceaf863
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-01 04:42:16 +0100

    Add monke test file: simple-python-module-1be8d785.py

---
+++ simple-python-module-1be8d785.py
@@ -0,0 +1,14 @@
+"""A Python module containing one simple function and one simple class, including the token '1be8d785'. | token=1be8d785"""
+
+def greet(name):
+    """Return a greeting message for the given name. Token: 1be8d785."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class. Token: 1be8d785."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
