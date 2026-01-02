40ce91de3f9927489ef7df2b84c9851686084550
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-02 23:09:44 +0100

    Add monke test file: simple-python-module-f4f497da.py

---
+++ simple-python-module-f4f497da.py
@@ -0,0 +1,18 @@
+"""A Python module containing a simple function and a simple class with the token 'f4f497da' in a docstring. | token=f4f497da"""
+
+def greet(name):
+    """Return a greeting message with the given name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class with token f4f497da included in the docstring."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
+
+    def reset(self):
+        self.count = 0
+
