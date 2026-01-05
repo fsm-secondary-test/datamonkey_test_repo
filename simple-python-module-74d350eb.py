723fba0d092200906c174f2b124bda50544b338d
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-05 19:56:50 +0100

    Add monke test file: simple-python-module-74d350eb.py

---
+++ simple-python-module-74d350eb.py
@@ -0,0 +1,13 @@
+"""A small Python module containing one function and one class, including the token '74d350eb' in a docstring. | token=74d350eb"""
+
+def greet(name):
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class holding the token '74d350eb'."""
+    TOKEN = '74d350eb'
+
+    def get_token(self):
+        """Return the token."""
+        return self.TOKEN
