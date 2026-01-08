e9950f5f46146c2294c594f4aae5b38acebfb46d
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-08 09:30:47 +0100

    Add monke test file: simple-module-with-function-and-class-df67204b.py

---
+++ simple-module-with-function-and-class-df67204b.py
@@ -0,0 +1,16 @@
+"""A Python module containing one simple function and one simple class. Includes the token 'df67204b' in a docstring. | token=df67204b"""
+
+def greet(name):
+    """Return a greeting message including the given name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """Simple counter class with token df67204b in docstring."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def value(self):
+        return self.count
