274856e23b20c81882f861cb8cb8e87972a7ea05
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-09 03:24:31 +0100

    Add monke test file: simple-python-module-ca22161a.py

---
+++ simple-python-module-ca22161a.py
@@ -0,0 +1,12 @@
+"""A small Python module with one function and one class containing the token 'ca22161a'. | token=ca22161a"""
+
+def greet(name):
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class holding a special token 'ca22161a' as a class attribute."""
+    token = 'ca22161a'
+
+    def get_token(self):
+        return self.token
