3f49d8fbf6b82f8a1896b22b3175c694d80d3061
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-10 15:05:28 +0100

    Add monke test file: simple-python-module-2474c4c0.py

---
+++ simple-python-module-2474c4c0.py
@@ -0,0 +1,17 @@
+"""A Python module containing one simple function and one simple class, including the token '2474c4c0' in a docstring. | token=2474c4c0"""
+
+def greet(name):
+    """Return a greeting message including the name. Token: 2474c4c0"""
+    return f"Hello, {name}!"
+
+class Counter:
+    """Simple counter class with token 2474c4c0 in docstring."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def value(self):
+        return self.count
+
