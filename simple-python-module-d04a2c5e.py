df4cce15d21751f94feeb6bbfa9b7b69b6d18c63
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-10 17:11:21 +0100

    Add monke test file: simple-python-module-d04a2c5e.py

---
+++ simple-python-module-d04a2c5e.py
@@ -0,0 +1,13 @@
+"""A Python module containing one function and one class, including the token 'd04a2c5e'. | token=d04a2c5e"""
+
+def greet(name):
+    """Return a greeting message including the provided name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class that holds a special token as a string constant."""
+    TOKEN = 'd04a2c5e'
+
+    def get_token(self):
+        """Return the stored token."""
+        return self.TOKEN
