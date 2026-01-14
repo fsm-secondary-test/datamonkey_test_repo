fd13a4abb84cc01eb47650019af0edbbb6c2e852
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-15 00:31:58 +0100

    Add monke test file: simple-python-module-ca178f4a.py

---
+++ simple-python-module-ca178f4a.py
@@ -0,0 +1,14 @@
+"""A small Python module with one function and one class, including the token 'ca178f4a' in a docstring. | token=ca178f4a"""
+
+def greet(name):
+    """Return a greeting message for the given name. Token: ca178f4a"""
+    return f"Hello, {name}!"
+
+class Counter:
+    """Simple counter class. Token: ca178f4a"""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
