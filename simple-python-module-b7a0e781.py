79cb1c0894bdb5e93b877dd620c396c0b68fec31
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-20 16:18:38 +0100

    Add monke test file: simple-python-module-b7a0e781.py

---
+++ simple-python-module-b7a0e781.py
@@ -0,0 +1,16 @@
+"""This module contains a simple function and a simple class, including the token 'b7a0e781' in a docstring. | token=b7a0e781"""
+
+def greet(name):
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """Simple counter class with token b7a0e781."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def value(self):
+        return self.count
