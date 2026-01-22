18e1060df7d9c4df3df8cc1eb54177da0f981ef0
fsm-secondary-test <fsmsecondary@gmail.com>
2026-01-22 11:58:49 +0100

    Update monke test file: simple-python-module-c69d37ee.py

--- simple-python-module-c69d37ee.py
+++ simple-python-module-c69d37ee.py
@@ -1,17 +1,13 @@
-"""A Python module containing one simple function and one simple class, including the token 'c69d37ee' in a docstring. | token=c69d37ee"""
+"""A small Python module with one simple function and one simple class, including the token 'c69d37ee'. | token=c69d37ee"""
 
 def greet(name):
-    """Return a greeting message including the token c69d37ee."""
-    return f"Hello, {name}! Token: c69d37ee"
+    """Return a greeting message for the given name."""
+    return f"Hello, {name}!"
 
-class Counter:
-    """A simple counter class with token c69d37ee in the docstring."""
-    def __init__(self):
-        self.count = 0
-    
-    def increment(self):
-        self.count += 1
-        return self.count
-    
-    def reset(self):
-        self.count = 0
+class TokenHolder:
+    """Class that holds a special token: c69d37ee."""
+    TOKEN = "c69d37ee"
+
+    def get_token(self):
+        """Return the special token."""
+        return self.TOKEN
