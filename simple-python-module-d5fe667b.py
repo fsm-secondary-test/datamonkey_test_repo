7d919c5fff61439a180710ba555b18690b7ee55d
fsm-secondary-test <fsmsecondary@gmail.com>
2026-02-03 05:49:19 +0100

    Add monke test file: simple-python-module-d5fe667b.py

---
+++ simple-python-module-d5fe667b.py
@@ -0,0 +1,20 @@
+"""A small Python module with one function and one class, including the token 'd5fe667b'. | token=d5fe667b"""
+
+def greet(name):
+    """Return a greeting message including the token 'd5fe667b'."""
+    return f"Hello, {name}! Your token is d5fe667b."
+
+class Counter:
+    """Simple counter class that uses token 'd5fe667b' internally."""
+    def __init__(self):
+        self.count = 0
+        self.token = 'd5fe667b'
+
+    def increment(self):
+        self.count += 1
+
+    def get_count(self):
+        return self.count
+
+    def get_token(self):
+        return self.token
