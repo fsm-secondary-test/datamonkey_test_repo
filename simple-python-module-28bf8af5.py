90462f7c507230dc2a79d2a2d74df55c67fd5250
fsm-secondary-test <fsmsecondary@gmail.com>
2026-02-06 21:22:44 +0100

    Add monke test file: simple-python-module-28bf8af5.py

---
+++ simple-python-module-28bf8af5.py
@@ -0,0 +1,19 @@
+"""A small Python module containing one function and one class, including the token '28bf8af5' in a docstring. | token=28bf8af5"""
+
+def greet(name):
+    """Return a greeting message including the name. Token: 28bf8af5"""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class. Token inside docstring: 28bf8af5"""
+
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        """Increment the count by 1."""
+        self.count += 1
+
+    def get_count(self):
+        """Return the current count."""
+        return self.count
