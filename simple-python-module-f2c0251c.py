7898d06db3cc7c1f7ff3385a6a74d4752e87ebb2
fsm-secondary-test <fsmsecondary@gmail.com>
2026-02-12 12:04:17 +0100

    Add monke test file: simple-python-module-f2c0251c.py

---
+++ simple-python-module-f2c0251c.py
@@ -0,0 +1,19 @@
+"""A small Python module with a simple function and a simple class, including the token 'f2c0251c'. | token=f2c0251c"""
+
+def greet(name):
+    """Return a greeting message for the given name. Token: f2c0251c"""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class. Token: f2c0251c"""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def reset(self):
+        self.count = 0
+
+    def get_count(self):
+        return self.count
