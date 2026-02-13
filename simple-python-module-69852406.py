7302708f514adec6d9228d779e7007d55a2b8786
fsm-secondary-test <fsmsecondary@gmail.com>
2026-02-13 19:31:12 +0100

    Update monke test file: simple-python-module-69852406.py

--- simple-python-module-69852406.py
+++ simple-python-module-69852406.py
@@ -1,18 +1,19 @@
-"""A small Python module with one simple function and one simple class, including the token '69852406' in a docstring. | token=69852406"""
+"""A Python module with one function and one class, including the token '69852406' in a docstring. | token=69852406"""
 
 def greet(name):
-    """Return a greeting message with the given name. Token: 69852406"""
-    return f"Hello, {name}!"
+    """Return a greeting message including the token 69852406."""
+    return f"Hello, {name}! Token: 69852406"
 
 class Counter:
-    """A simple counter class. Token: 69852406"""
+    """A simple counter class.
 
+    Token: 69852406
+    """
     def __init__(self):
         self.count = 0
 
     def increment(self):
         self.count += 1
-        return self.count
 
-    def reset(self):
-        self.count = 0
+    def get_count(self):
+        return self.count
