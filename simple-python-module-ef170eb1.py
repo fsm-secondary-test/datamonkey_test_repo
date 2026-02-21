0fee5adcad211aae48cf57bbed769d5ecbf8dd85
fsm-secondary-test <fsmsecondary@gmail.com>
2026-02-21 10:17:25 -0800

    Add monke test file: simple-python-module-ef170eb1.py

---
+++ simple-python-module-ef170eb1.py
@@ -0,0 +1,18 @@
+"""A small Python module containing one function and one class, including the token 'ef170eb1' in a docstring. | token=ef170eb1"""
+
+def greet(name):
+    """Return a greeting message for the given name. Token: ef170eb1"""
+    return f"Hello, {name}!"
+
+class Counter:
+    """Simple counter class. Token: ef170eb1"""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        """Increment the counter by 1."""
+        self.count += 1
+
+    def value(self):
+        """Return the current count value."""
+        return self.count
