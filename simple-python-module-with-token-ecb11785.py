213dae31f639cea4582405d1341af40f1897879a
fsm-secondary-test <fsmsecondary@gmail.com>
2026-02-25 12:02:18 -0800

    Add monke test file: simple-python-module-with-token-ecb11785.py

---
+++ simple-python-module-with-token-ecb11785.py
@@ -0,0 +1,14 @@
+"""A small Python module containing one simple function and one simple class. Includes the token 'ecb11785' in a docstring. | token=ecb11785"""
+
+def greet(name):
+    """Return a greeting message."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """Simple counter class with token ecb11785."""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
