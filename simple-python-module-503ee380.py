2c80b5d3c7f73dd24d5bcbaa7ece6d1df7dc8aa3
fsm-secondary-test <fsmsecondary@gmail.com>
2026-02-27 15:23:26 -0800

    Add monke test file: simple-python-module-503ee380.py

---
+++ simple-python-module-503ee380.py
@@ -0,0 +1,12 @@
+"""A small Python module containing one function and one class. Includes the token '503ee380' in the class docstring. | token=503ee380"""
+
+def greet(name):
+    """Return a greeting message for the given name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """A simple class holding the token 503ee380 as a constant."""
+    TOKEN = '503ee380'
+
+    def get_token(self):
+        return self.TOKEN
