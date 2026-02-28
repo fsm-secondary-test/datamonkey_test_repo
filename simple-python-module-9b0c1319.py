14bad0c78960de85ac6041f65f962f8c226f2b19
fsm-secondary-test <fsmsecondary@gmail.com>
2026-02-27 17:38:08 -0800

    Add monke test file: simple-python-module-9b0c1319.py

---
+++ simple-python-module-9b0c1319.py
@@ -0,0 +1,16 @@
+"""A small Python module with one function and one class, including the token '9b0c1319'. | token=9b0c1319"""
+
+def greet(name):
+    """Return a greeting message including the token 9b0c1319."""
+    return f"Hello, {name}! Token: 9b0c1319"
+
+class Counter:
+    """Simple counter class that uses token 9b0c1319 internally."""
+    
+    def __init__(self):
+        self.count = 0
+        self.token = '9b0c1319'
+    
+    def increment(self):
+        self.count += 1
+        return self.count
