0f7d3cbe176b0218e2f55c452f63b7adaa33946e
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-04 01:02:42 -0800

    Update monke test file: simple-python-module-c41ef150.py

--- simple-python-module-c41ef150.py
+++ simple-python-module-c41ef150.py
@@ -1,15 +1,18 @@
-"""A small Python module containing one function and one class, including the token 'c41ef150'. | token=c41ef150"""
+"""A module with one function and one class, including the token c41ef150 in a docstring. | token=c41ef150"""
 
 def greet(name):
-    """Return a greeting message including the token c41ef150."""
-    return f"Hello, {name}! Your token is c41ef150."
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
 
-class Counter:
-    """A simple counter class that includes the token c41ef150."""
+class TokenHolder:
+    """Class to hold a token string.
 
-    def __init__(self, start=0):
-        self.count = start
+    Token: c41ef150
+    """
 
-    def increment(self):
-        self.count += 1
-        return self.count
+    def __init__(self):
+        self.token = "c41ef150"
+
+    def get_token(self):
+        """Return the stored token."""
+        return self.token
