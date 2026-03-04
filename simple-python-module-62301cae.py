1a5d64868cfd96534aaaa42aa5f5d461fea2ebc2
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-03 16:02:44 -0800

    Add monke test file: simple-python-module-62301cae.py

---
+++ simple-python-module-62301cae.py
@@ -0,0 +1,20 @@
+"""A small Python module containing one function and one class, including the token '62301cae' in a docstring. | token=62301cae"""
+
+def greet(name):
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class.
+
+    Token: 62301cae
+    """
+    def __init__(self):
+        self.count = 0
+    
+    def increment(self):
+        self.count += 1
+        return self.count
+    
+    def reset(self):
+        self.count = 0
