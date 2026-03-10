dcd7eeaab9ab0e94fa7ef10c271b28f59f131c16
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-09 18:54:40 -0700

    Add monke test file: simple-python-module-37e023de.py

---
+++ simple-python-module-37e023de.py
@@ -0,0 +1,14 @@
+"""A small Python module containing one function and one class, including the token '37e023de'. | token=37e023de"""
+
+def greet(name):
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class holding a special token: 37e023de"""
+
+    def __init__(self):
+        self.token = "37e023de"
+
+    def get_token(self):
+        return self.token
