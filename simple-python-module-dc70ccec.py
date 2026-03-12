eb6c14b06425491d9393d2f0a0863ee9bbcc1764
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-11 18:22:44 -0700

    Add monke test file: simple-python-module-dc70ccec.py

---
+++ simple-python-module-dc70ccec.py
@@ -0,0 +1,14 @@
+"""A small Python module with one function and one class, including the token 'dc70ccec' in a docstring. | token=dc70ccec"""
+
+def greet(name):
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
+
+class Counter:
+    """A simple counter class that increments a count. Token: dc70ccec"""
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
