6ec656bc77275aa2d33111cdcad7b3429ce4ce12
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-11 19:55:13 -0700

    Add monke test file: simple-python-module-880f0257.py

---
+++ simple-python-module-880f0257.py
@@ -0,0 +1,19 @@
+"""A small Python module containing one function and one class, including the token '880f0257'. | token=880f0257"""
+
+def greet(name):
+    """Return a greeting message including the token 880f0257."""
+    return f"Hello, {name}! Token: 880f0257"
+
+class Counter:
+    """A simple counter class with token 880f0257 in docstring."""
+
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        """Increment the counter by 1."""
+        self.count += 1
+
+    def get_count(self):
+        """Return the current count."""
+        return self.count
