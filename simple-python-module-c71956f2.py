9bfbfb6691acccbfd727f28ec105e990402675ce
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-13 18:13:50 -0700

    Add monke test file: simple-python-module-c71956f2.py

---
+++ simple-python-module-c71956f2.py
@@ -0,0 +1,14 @@
+"""A small Python module with one simple function and one simple class, including the token 'c71956f2' in a docstring. | token=c71956f2"""
+
+def greet(name):
+    """Return a greeting message for the given name. Token: c71956f2"""
+    return f"Hello, {name}!"
+
+class Person:
+    """A simple Person class with a name attribute. Token: c71956f2"""
+
+    def __init__(self, name):
+        self.name = name
+
+    def introduce(self):
+        return f"Hi, my name is {self.name}."
