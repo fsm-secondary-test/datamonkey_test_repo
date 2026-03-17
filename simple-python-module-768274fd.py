3f9b0f38416bbe3655ce86afc1284cbe1f229181
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-17 08:01:44 -0700

    Update monke test file: simple-python-module-768274fd.py

--- simple-python-module-768274fd.py
+++ simple-python-module-768274fd.py
@@ -1,15 +1,13 @@
-"""A simple Python module containing one function and one class with the literal token '768274fd' included in a docstring. | token=768274fd"""
+"""A small Python module containing one simple function and one simple class with the token '768274fd' in a docstring. | token=768274fd"""
 
 def greet(name):
-    """Return a greeting message including the token 768274fd."""
-    return f"Hello, {name}! Your token is 768274fd."
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
 
-class Counter:
-    """A simple counter class that increments a value. Token: 768274fd."""
+class TokenHolder:
+    """Class holding the token '768274fd' as a class attribute."""
+    token = '768274fd'
 
-    def __init__(self):
-        self.count = 0
-
-    def increment(self):
-        self.count += 1
-        return self.count
+    def get_token(self):
+        """Return the stored token."""
+        return self.token
