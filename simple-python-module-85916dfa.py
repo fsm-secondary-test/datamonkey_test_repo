2fa9b53844dda4396a07457e715381fd30b632a6
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-17 09:16:36 -0700

    Add monke test file: simple-python-module-85916dfa.py

---
+++ simple-python-module-85916dfa.py
@@ -0,0 +1,17 @@
+"""A Python module containing one simple function and one simple class, including the token '85916dfa'. | token=85916dfa"""
+
+def greet(name):
+    """Return a greeting message including the given name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class that holds a special token.
+
+    Token: 85916dfa
+    """
+    def __init__(self):
+        self.token = '85916dfa'
+
+    def get_token(self):
+        """Return the stored token."""
+        return self.token
