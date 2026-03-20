a69e8c69e14bbc5bb55b95ea2685211caa156821
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-20 11:45:54 -0700

    Add monke test file: sample-python-module-e7afb760.py

---
+++ sample-python-module-e7afb760.py
@@ -0,0 +1,14 @@
+"""A small Python module containing one function and one class with the token 'e7afb760' included in a docstring. | token=e7afb760"""
+
+def greet(name):
+    """Return a greeting message including the given name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class that holds the special token 'e7afb760'."""
+    def __init__(self):
+        self.token = 'e7afb760'
+
+    def get_token(self):
+        """Return the stored token."""
+        return self.token
