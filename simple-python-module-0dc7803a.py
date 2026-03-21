77c5fa31fb1e466b95b60fdda8ac611de70a9e14
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-21 10:59:01 -0700

    Add monke test file: simple-python-module-0dc7803a.py

---
+++ simple-python-module-0dc7803a.py
@@ -0,0 +1,15 @@
+"""A small Python module with one function and one class, including the token '0dc7803a' in a docstring. | token=0dc7803a"""
+
+def greet(name):
+    """Return a greeting message including the name provided."""
+    return f"Hello, {name}!"
+
+class SimpleCounter:
+    """A simple counter class that increments a count. Token: 0dc7803a"""
+
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+        return self.count
