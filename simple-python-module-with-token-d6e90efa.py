220ba9fe46a9633932e7c15c3af644a6fcebcb17
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-23 15:09:37 -0700

    Add monke test file: simple-python-module-with-token-d6e90efa.py

---
+++ simple-python-module-with-token-d6e90efa.py
@@ -0,0 +1,13 @@
+"""A small Python module containing one function and one class, including the token 'd6e90efa' in a docstring. | token=d6e90efa"""
+
+def greet(name):
+    """Return a greeting message including the name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class holding a special token: d6e90efa"""
+    TOKEN = "d6e90efa"
+
+    def get_token(self):
+        """Return the stored token."""
+        return self.TOKEN
