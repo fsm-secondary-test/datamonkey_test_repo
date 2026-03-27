9ac7f2cb2dae24ee1f7c805e89f52a498cf48805
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-27 05:28:31 -0700

    Add monke test file: simple-python-module-42b04f33.py

---
+++ simple-python-module-42b04f33.py
@@ -0,0 +1,15 @@
+"""A small Python module with one simple function and one simple class, including the literal token '42b04f33'. | token=42b04f33"""
+
+def greet(name):
+    """Return a greeting message including the given name."""
+    return f"Hello, {name}!"
+
+class Answer:
+    """Class to represent the ultimate answer with token '42b04f33'."""
+    def __init__(self):
+        self.value = 42
+        self.token = '42b04f33'
+    
+    def get_answer(self):
+        """Return the ultimate answer value."""
+        return self.value
