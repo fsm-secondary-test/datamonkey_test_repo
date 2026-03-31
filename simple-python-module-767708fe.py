e18263715f3935bb9739968d9624a48160f3ba1c
fsm-secondary-test <fsmsecondary@gmail.com>
2026-03-31 04:35:58 -0700

    Add monke test file: simple-python-module-767708fe.py

---
+++ simple-python-module-767708fe.py
@@ -0,0 +1,13 @@
+"""A small Python module containing one function and one class, including the token '767708fe' in a docstring. | token=767708fe"""
+
+def greet(name):
+    """Return a greeting message including the given name."""
+    return f"Hello, {name}!"
+
+class TokenHolder:
+    """Class that holds a token string '767708fe' as a constant."""
+    TOKEN = '767708fe'
+
+    def get_token(self):
+        """Return the stored token."""
+        return self.TOKEN
